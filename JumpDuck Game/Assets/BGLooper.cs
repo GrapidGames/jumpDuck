de1e55c349270195bf7275fd555c7acf28362b80
Jimmy Pease <jimmy.pease.21@gmail.com>
2015-06-13 18:36:21 -0400

    the game

---
+++ JumpDuck Game/Assets/BGLooper.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BGLooper : MonoBehaviour {
+
+	int numBGPanels = 5;
+
+
+	void OnTriggerEnter2D(Collider2D collider){
+		Debug.Log ("Triggered: " + collider.name);
+
+		float width = ((BoxCollider2D)collider).size.x;
+
+		Vector3 pos = collider.transform.position;
+
+		pos.x += width * numBGPanels - width/4;
+
+		collider.transform.position = pos;
+	}
+}
---
+++ JumpDuck Game/Assets/playerMovement.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerMovement : MonoBehaviour {
+
+	Vector3 velocity = Vector3.zero;
+	public Vector3 gravity;
+	public Vector3 jumpVelocity;
+	float forwardSpeed = 1f;
+
+	bool jump = false;
+	bool canJump = false;
+
+	// Use this for initialization
+	void Start () {
+	
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	
+		if (transform.position.y == 0) {
+			if (Input.GetKeyDown (KeyCode.Space) || Input.GetMouseButtonDown (0))
+				jump = true;
+		}
+	}
+	
+	void FixedUpdate() {
+		velocity.x = forwardSpeed;
+		velocity += gravity * Time.deltaTime;
+
+		if (jump == true) {
+			jump = false;
+			velocity += jumpVelocity;
+		}
+
+		transform.position += velocity * Time.deltaTime;
+
+		if (transform.position.y < 0f) {
+			velocity.y = 0;
+			transform.position += new Vector3(0, -transform.position.y, 0);
+		}
+	}
+}
